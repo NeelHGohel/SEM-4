e78c5749e4baf2e68914f7b17c88fcb404a5ec23
neelgohelh <neelgohel8887@gmail.com>
2024-12-04 17:40:41 +0530

    04-12-2024

--- DBMS 2/LAB 1.sql
+++ DBMS 2/LAB 1.sql
@@ -95,29 +95,74 @@ WHERE Artists.Artist_name = 'Shreya Ghoshal';
 
 
 --22. Retrieve all song titles by artists who have more than one album.
+SELECT DISTINCT SONGS.SONG_TITLE, ARTISTS.ARTIST_ID
+FROM SONGS 
+INNER JOIN ALBUMS ON SONGS.ALBUM_ID = ALBUMS.ALBUM_ID
+INNER JOIN ARTISTS ON ARTISTS.ARTIST_ID = ALBUMS.ARTIST_ID
+GROUP BY ARTISTS.ARTIST_ID,SONGS.SONG_TITLE
+HAVING COUNT(ALBUMS.ALBUM_ID) > 1;
 
 
 --23. Retrieve all albums along with the total number of songs.
+SELECT ALBUMS.ALBUM_TITLE, COUNT(SONGS.SONG_TITLE) AS TOTAL_SONGS
+FROM ALBUMS
+INNER JOIN SONGS ON ALBUMS.ALBUM_ID = SONGS.ALBUM_ID
+GROUP BY ALBUMS.ALBUM_ID,ALBUMS.ALBUM_TITLE;
 
 
 --24. Retrieve all songs and release year and sort them by release year.
+SELECT SONGS.SONG_TITLE, ALBUMS.RELEASE_YEAR
+FROM SONGS
+INNER JOIN ALBUMS ON SONGS.ALBUM_ID = ALBUMS.ALBUM_ID
+ORDER BY ALBUMS.RELEASE_YEAR;
 
 
 --25. Retrieve the total number of songs for each genre, showing genres that have more than 2 songs.
+SELECT Genre, COUNT(Song_title) AS TOTAL_SONGS
+FROM Songs
+GROUP BY Genre
+HAVING COUNT(Song_title) > 2;
+
 
 
 --26. List all artists who have albums that contain more than 3 songs.
+SELECT Artists.Artist_name
+FROM Artists
+INNER JOIN Albums ON Artists.Artist_id = Albums.Artist_id
+INNER JOIN Songs ON Albums.Album_id = Songs.Album_id
+GROUP BY Artists.Artist_id, Albums.Album_id,Artists.Artist_name
+HAVING COUNT(Songs.Song_title) > 3;
 
 
 --Part – C
 
 --27. Retrieve albums that have been released in the same year as 'Album4'
+SELECT Albums.Album_title, Albums.Release_year
+FROM Albums
+WHERE Albums.Release_year = (
+    SELECT Albums.Release_year
+    FROM Albums
+    WHERE Albums.Album_title= 'Album4'
+);
 
 
 --28. Find the longest song in each genre
+SELECT Genre, Song_title, MAX(Duration) AS LONGEST_DURATION
+FROM Songs
+GROUP BY Genre,Song_title;
 
 
 --29. Retrieve the titles of songs released in albums that contain the word 'Album' in the title.
+SELECT Songs.Song_title
+FROM Songs
+INNER JOIN Albums ON Songs.Album_id= Albums.Album_id
+WHERE Albums.Album_title LIKE '%Album%';
 
 
---30. Retrieve the total duration of songs by each artist where total duration exceeds 15 minutes.
+--30. Retrieve the total duration of songs by each artist where total duration exceeds 15 minutes.SELECT Artists.Artist_name, SUM(Songs.Duration) AS TOTAL_DURATION
+FROM Artists
+INNER JOIN Albums ON Artists.Artist_id= Albums.Artist_id
+INNER JOIN Songs ON Albums.Album_id = Songs.Album_id
+GROUP BY Artists.Artist_id,Artists.Artist_name
+HAVING SUM(Songs.Duration) > 15;
+
--- Flutter/lab_3/element_appears_only_one.dart
+++ Flutter/lab_3/element_appears_only_one.dart
@@ -4,9 +4,12 @@ void main(){
   stdout.write("Enter numbers and enter -1 to exit ");
   int n1 = int.parse(stdin.readLineSync()!);
 
-  Map<int,bool> map = {};
+  List<int> l1 = [];
   while(n1 != -1){
+    l1.add(n1);
     stdout.write("Enter numbers and enter -1 to exit ");
     n1 = int.parse(stdin.readLineSync()!);
   }
+  
+  print(l1);
 }
