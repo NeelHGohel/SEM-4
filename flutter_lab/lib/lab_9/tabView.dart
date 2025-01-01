29e135f6a5f1dbe2ce8b84486b607e5526e7a563
neelgohelh <neelgohel8887@gmail.com>
2025-01-01 11:23:31 +0530

    01-01-2025

--- flutter_lab/lib/lab_9/tabView.dart
+++ flutter_lab/lib/lab_9/tabView.dart
@@ -22,8 +22,54 @@ class Tabview extends StatelessWidget {
               )
             ],
           ),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showDialog(
+                      context: context,
+                      builder: (context) {
+                        return AlertDialog(
+                          title: Text("Are you sure logout ?"),
+                          actions: [
+                            ElevatedButton(onPressed: () {}, child: Text("Ok")),
+                            ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                },
+                                child: Text("Cancle"))
+                          ],
+                        );
+                      });
+                },
+                icon: Icon(Icons.logout))
+          ],
         ),
         body: TabBarView(children: [Home(), About()]),
+        drawer: Drawer(
+          child: Column(
+            children: [
+              DrawerHeader(
+                  child: Image.network(
+                      "https://static.vecteezy.com/system/resources/previews/012/667/615/non_2x/go-to-web-icon-for-your-web-design-logo-infographic-ui-vector.jpg")),
+              ListTile(
+                title: Text("Home"),
+                leading: Icon(Icons.home),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => Home()));
+                },
+              ),
+              ListTile(
+                title: Text("About"),
+                leading: Icon(Icons.arrow_back),
+                onTap: () {
+                  Navigator.of(context)
+                      .push(MaterialPageRoute(builder: (context) => About()));
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
