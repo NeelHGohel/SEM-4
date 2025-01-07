4b7885641e0b3b534a7eb0105c087099f32861f9
neelgohelh <neelgohel8887@gmail.com>
2025-01-07 13:37:42 +0530

    07-01-2025

--- flutter_lab/lib/lab_10/login_screen.dart
+++ flutter_lab/lib/lab_10/login_screen.dart
@@ -9,6 +9,9 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   bool isHide = true;
+  bool isRegisterScreen = false;
+  var loginText = "Login";
+  var registerText = "Register";
   GlobalKey<FormState> _key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
@@ -68,13 +71,69 @@ class _LoginScreenState extends State<LoginScreen> {
               SizedBox(
                 height: 30,
               ),
+              Visibility(
+                visible: isRegisterScreen,
+                child: Container(
+                  child: TextFormField(
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return "Enter confirm PassWord";
+                      }
+                      return null;
+                    },
+                    obscureText: isHide,
+                    decoration: InputDecoration(
+                      hintText: "Enter confirm PassWord",
+                      labelText: "Enter confirm PassWord",
+                      suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isHide = !isHide;
+                            });
+                          },
+                          icon: Icon(isHide
+                              ? Icons.visibility_sharp
+                              : Icons.visibility_off_outlined)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
               ElevatedButton(
                 onPressed: () {
                   if (_key.currentState!.validate()) {}
                 },
-                child: Text("Log in"),
+                child: Text(isRegisterScreen ? registerText : loginText),
               ),
-
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("Are you Registered user "),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  InkWell(
+                    onTap: () {
+                      isRegisterScreen = !isRegisterScreen;
+                      loginText = isRegisterScreen ? registerText : loginText;
+                      registerText = isRegisterScreen ? registerText : loginText;
+                      setState(() {});
+                    },
+                    child: Text(
+                      registerText,
+                      style: TextStyle(color: Colors.blue),
+                    ),
+                  )
+                ],
+              )
             ],
           ),
         ),
