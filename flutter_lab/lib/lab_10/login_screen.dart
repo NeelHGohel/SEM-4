322cc144ee5af99e2fb6fd6096a931ff50965048
neelgohelh <neelgohel8887@gmail.com>
2025-01-07 12:48:20 +0530

    07-01-2025

--- flutter_lab/lib/lab_10/login_screen.dart
+++ flutter_lab/lib/lab_10/login_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  const LoginScreen({super.key});
+  LoginScreen({super.key});
 
+  GlobalKey<FormState> _key = new GlobalKey<FormState>();
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController passWordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      
-    )
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _key,
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              validator: (value) {
+                if (value!.isEmpty) {
+                  return "Enter Name";
+                }
+                return null;
+              },
+              controller: nameController,
+              decoration: InputDecoration(
+                  hintText: "Enter Name",
+                  labelText: "Enter Name",
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30))),
+            )
+          ],
+        )));
   }
 }
