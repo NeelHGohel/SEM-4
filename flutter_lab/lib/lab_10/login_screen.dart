0881ab283cd62db792f6a011f9da29262a79bb48
neelgohelh <neelgohel8887@gmail.com>
2025-01-07 13:06:47 +0530

    07-01-2025

--- flutter_lab/lib/lab_10/login_screen.dart
+++ flutter_lab/lib/lab_10/login_screen.dart
@@ -1,41 +1,84 @@
 import 'package:flutter/material.dart';
 
 class LoginScreen extends StatefulWidget {
-  LoginScreen({super.key});
+  const LoginScreen({super.key});
 
-  GlobalKey<FormState> _key = new GlobalKey<FormState>();
   @override
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passWordController = TextEditingController();
-
+  bool isHide = true;
+  GlobalKey<FormState> _key = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
-    return Padding(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Form"),
+      ),
+      body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Form(
           key: _key,
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return "Enter Name";
-                }
-                return null;
-              },
-              controller: nameController,
-              decoration: InputDecoration(
-                  hintText: "Enter Name",
-                  labelText: "Enter Name",
+          child: Column(
+            children: [
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter Name";
+                  }
+                  return null;
+                },
+                decoration: InputDecoration(
+                  hintText: "Enter name",
+                  labelText: "Enter name",
                   border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30))),
-            )
-          ],
-        )));
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return "Enter PassWord";
+                  }
+                  return null;
+                },
+                obscureText: isHide,
+                decoration: InputDecoration(
+                  hintText: "Enter PassWord",
+                  labelText: "Enter PassWord",
+                  suffixIcon: IconButton(
+                      onPressed: () {
+                        setState(() {
+                          isHide = !isHide;
+                        });
+                      },
+                      icon: Icon(isHide
+                          ? Icons.visibility_sharp
+                          : Icons.visibility_off_outlined)),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  if (_key.currentState!.validate()) {}
+                },
+                child: Text("Log in"),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
