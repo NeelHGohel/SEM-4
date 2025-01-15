255d1d4208f6e6b366a12e57ad2add4e14d6245d
neelgohelh <neelgohel8887@gmail.com>
2025-01-15 10:45:44 +0530

    15-01-2024

---
+++ flutter_lab/lib/lab_11/both_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class BothView extends StatefulWidget {
+  const BothView({super.key});
+
+  @override
+  State<BothView> createState() => _BothViewState();
+}
+
+class _BothViewState extends State<BothView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
