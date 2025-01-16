bbeb59b1796e24d24ae462035dda90415b4353de
neelgohelh <neelgohel8887@gmail.com>
2025-01-16 16:15:17 +0530

    16-01-2025

---
+++ DBMS 2/LAB 05.sql
@@ -0,0 +1,58 @@
+-- Creating PersonInfo Table
+CREATE TABLE PersonInfo (
+ PersonID INT PRIMARY KEY,
+ PersonName VARCHAR(100) NOT NULL,
+ Salary DECIMAL(8,2) NOT NULL,
+ JoiningDate DATETIME NULL,
+ City VARCHAR(100) NOT NULL,
+ Age INT NULL,
+ BirthDate DATETIME NOT NULL
+);
+
+
+-- Creating PersonLog Table
+CREATE TABLE PersonLog (
+ PLogID INT PRIMARY KEY IDENTITY(1,1),
+ PersonID INT NOT NULL,
+ PersonName VARCHAR(250) NOT NULL,
+ Operation VARCHAR(50) NOT NULL,
+ UpdateDate DATETIME NOT NULL,
+ FOREIGN KEY (PersonID) REFERENCES PersonInfo(PersonID) ON DELETE CASCADE
+);
+
+
+--Part – A
+--1. Create a trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo table to display
+--a message “Record is Affected.”
+CREATE OR ALTER TRIGGER TR_A1
+ON PersonInfo
+AFTER INSERT , UPDATE , DELETE
+AS
+BEGIN
+	PRINT 'RECORD IS AFFECTED'
+END;
+
+SELECT * FROM PersonInfo;
+INSERT INTO PersonInfo VALUES(1 , 'NEEL' , 1111.11 , '2025-01-16' ,'RAJKOT' , 19 , '2005-11-30');
+DELETE FROM PersonInfo WHERE PersonID = 1;
+
+--2. Create a trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo table. For that,
+--log all operations performed on the person table into PersonLog.
+
+
+
+--3. Create an INSTEAD OF trigger that fires on INSERT, UPDATE and DELETE operation on the PersonInfo
+--table. For that, log all operations performed on the person table into PersonLog.
+--4. Create a trigger that fires on INSERT operation on the PersonInfo table to convert person name into
+--uppercase whenever the record is inserted.
+--5. Create trigger that prevent duplicate entries of person name on PersonInfo table.
+--6. Create trigger that prevent Age below 18 years.
+--Part – B
+--7. Create a trigger that fires on INSERT operation on person table, which calculates the age and update
+--that age in Person table.
+--8. Create a Trigger to Limit Salary Decrease by a 10%.
+--Part – C
+--9. Create Trigger to Automatically Update JoiningDate to Current Date on INSERT if JoiningDate is NULL
+--during an INSERT.
+--10. Create DELETE trigger on PersonLog table, when we delete any record of PersonLog table it prints
+--‘Record deleted successfully from PersonLog’.
