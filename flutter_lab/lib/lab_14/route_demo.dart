829533a7cf278ef12c750b6ab1a8afc9a06fc49f
neelgohelh <neelgohel8887@gmail.com>
2025-01-29 10:29:20 +0530

    29-01-2025

---
+++ flutter_lab/lib/lab_14/route_demo.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class RouteDemo extends StatefulWidget {
+  const RouteDemo({super.key});
+
+  @override
+  State<RouteDemo> createState() => _RouteDemoState();
+}
+
+class _RouteDemoState extends State<RouteDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
